356732227437ae24ac0268a9fa7f5568c02b35bc
richard-miller-du <rmiller6@dcsdk12.org>
2024-10-21 10:01:07 -0600

    0.0.0.0f2
    Copied official version of LLMS-from-scratch code from github.

--- LLMs-from-scratch-main/ch05/08_memory_efficient_weight_loading/previous_chapters.py
+++ LLMs-from-scratch-main/ch05/08_memory_efficient_weight_loading/previous_chapters.py
@@ -4,15 +4,17 @@
 # Code: https://github.com/rasbt/LLMs-from-scratch
 #
 # This file collects all the relevant code that we covered thus far
-# throughout Chapters 2-4.
-# This file can be run as a standalone script.
+# throughout Chapters 2-5.
+
+
 import torch
 import torch.nn as nn
-
 
 #####################################
 # Chapter 3
 #####################################
+
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
@@ -166,29 +168,3 @@ class GPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
-
-
-def generate_text_simple(model, idx, max_new_tokens, context_size):
-    # idx is (B, T) array of indices in the current context
-    for _ in range(max_new_tokens):
-
-        # Crop current context if it exceeds the supported context size
-        # E.g., if LLM supports only 5 tokens, and the context size is 10
-        # then only the last 5 tokens are used as context
-        idx_cond = idx[:, -context_size:]
-
-        # Get the predictions
-        with torch.no_grad():
-            logits = model(idx_cond)
-
-        # Focus only on the last time step
-        # (batch, n_token, vocab_size) becomes (batch, vocab_size)
-        logits = logits[:, -1, :]
-
-        # Get the idx of the vocab entry with the highest logits value
-        idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
-
-        # Append sampled index to the running sequence
-        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
-
-    return idx
--- LLMs-from-scratch-main/ch05/README.md
+++ LLMs-from-scratch-main/ch05/README.md
@@ -1,9 +1,11 @@
 # Chapter 5: Pretraining on Unlabeled Data
 
+&nbsp;
 ## Main Chapter Code
 
 - [01_main-chapter-code](01_main-chapter-code) contains the main chapter code
 
+&nbsp;
 ## Bonus Materials
 
 - [02_alternative_weight_loading](02_alternative_weight_loading) contains code to load the GPT model weights from alternative places in case the model weights become unavailable from OpenAI
@@ -12,3 +14,4 @@
 - [05_bonus_hparam_tuning](05_bonus_hparam_tuning) contains an optional hyperparameter tuning script
 - [06_user_interface](06_user_interface) implements an interactive user interface to interact with the pretrained LLM
 - [07_gpt_to_llama](07_gpt_to_llama) contains a step-by-step guide for converting a GPT architecture implementation to Llama 3.2 and loads pretrained weights from Meta AI
+- [08_memory_efficient_weight_loading](08_memory_efficient_weight_loading) contains a bonus notebook showing how to load model weights via PyTorch's `load_state_dict` method more efficiently
